b93c030d9cd255556a818e980e3ab75f5e0db5db
Igor Rodionov <rodionov0504@gmail.com>
2024-12-06 18:14:13 +0100

    Add main scene with scene loading logic

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -1,1 +1,22 @@
+class_name GameManager
 extends Node
+
+@onready var game_container: Node = $GameContainer
+@onready var ui_container: CanvasLayer = $UIContainer
+
+var current_scene: Node = null
+
+func _ready():
+	load_scene("res://scenes/game.tscn")
+	load_ui("res://scenes/ui.tscn")
+
+func load_scene(path: String):
+	if current_scene:
+		current_scene.queue_free()
+	var scene = (load(path) as PackedScene).instantiate()
+	add_child(scene)
+	current_scene = scene
+
+func load_ui(path: String):
+	var ui_scene =  (load(path) as PackedScene).instantiate()
+	ui_container.add_child(ui_scene)
